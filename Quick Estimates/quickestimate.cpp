f1f98cb2fb155547c9f3b9215acb41775c23c203
Daniel Garza <danielg272313@gmail.com>
2025-01-20 17:31:45 +0000

    solve "Quick Estimates"

---
+++ Quick Estimates/quickestimate.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        string num;
+        cin >> num;
+        cout << num.length() << "\n";
+    }
+
+    return 0;
+}
